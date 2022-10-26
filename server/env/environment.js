845ad256a1f08746e669405115b20269c1eabf1b
Nati-Zerai <Natizerai11@gmail.com>
2022-10-26 18:58:27 +0400

    first comment

--- server/env/environment.js
+++ server/env/environment.js
@@ -1,6 +1,6 @@
-const cosmosPort = 10255; // replace with your port
-const dbName = 'walker123db';
-const key = '5GCmOwSbzCIG1lmap5qUBBg0wumSdMY7YaaBovjJaRoz5hCj0qRjggJBwr13S6SM09uFCBu6TyrXjROzOD4I7w==';
+// const cosmosPort = 10255; // replace with your port
+// const dbName = 'walker123db';
+// const key = '5GCmOwSbzCIG1lmap5qUBBg0wumSdMY7YaaBovjJaRoz5hCj0qRjggJBwr13S6SM09uFCBu6TyrXjROzOD4I7w==';
 
 module.exports = {
   cosmosPort,
